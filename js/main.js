1ba5087bc01324be0c5a0037cdb3c46330f0d622
Rey Espinosa <aztecrey3@gmail.com>
2018-03-03 13:50:21 -0600

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
 console.log("Up and Running");
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardsInPlay[0]);
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardsInPlay[1]);
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-	console.log("You found a match!");
-} else {
-	console.log("Sorry, try again!");
+checkForMatch = function() {
+	if (cardsInPlay === cards.length && cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again!");
+	};
 };
+flipCard = function(cardId) {
+	console.log("User flipped " + cardId);
+	cardsInPlay.push(cardId);
+	checkForMatch();
+};
+flipCard(cards[0]);
+flipCard(cards[2]);
