63a23f205663ec77c85a14118c2dd6bddff3d0cf
Rey Espinosa <aztecrey3@gmail.com>
2018-03-09 17:20:36 -0600

    added ramdomization

--- js/main.js
+++ js/main.js
@@ -23,24 +23,33 @@ var cards = [
 }
 ];
 var cardsInPlay = [];
+var score = 0;
+var flippedCards = document.getElementsByClassName('flipped');
 
 var checkForMatch = function() {
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
+		score += 1;
+		document.getElementById('score').textContent = score;
 	} else {
 		alert("Sorry, try again!");
 	}
-}
+};
 
 var flipCard = function() {
 		var cardId = this.getAttribute('data-id');
 		this.setAttribute('src', cards[cardId].cardImage);
+		this.setAttribute('class', 'flipped');
 		console.log("User flipped " + cards[cardId].rank);
 		cardsInPlay.push(cards[cardId].rank);
 		console.log(cards[cardId].cardImage);
 		console.log(cards[cardId].suit);
-		checkForMatch();	
-}
+		if (cardsInPlay.length == 2) {
+			checkForMatch();
+			cardsInPlay = [];
+		}
+};
+
 var createBoard = function() {
 	for (var i = 0; i < cards.length; i++) {
 	var cardElement = document.createElement('img');
@@ -49,5 +58,19 @@ var createBoard = function() {
 	cardElement.addEventListener('click', flipCard);
 	document.getElementById('gameboard').appendChild(cardElement);
 }
-}
+};
+
+var resetBoard = function() {
+	for (i = 0; i < flippedCards.length; i++) {
+		flippedCards[i].setAttribute('src', "images/back.png");
+		score = 0;
+		document.getElementById('score').textContent = 0;
+	}
+	for (var i = cards.length - 1; i > 0; i--) {
+		var j = Math.floor(Math.random() * (i + 1));
+		var temp = cards[i];
+		cards[i] = cards[j];
+		cards[j] = temp;
+	}
+};
 createBoard();
